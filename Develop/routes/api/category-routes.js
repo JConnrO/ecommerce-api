964239e3d7f5a39d7c64955e26e6688c762bbb43
j.connor.obrien@gmail.com <j.connor.obrien@gmail.com>
2021-07-29 15:27:19 -0400

    Product and Category Routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -17,30 +17,77 @@ router.get('/', (req, res) => {
     })
   // find all categories
   // be sure to include its associated Products
-
-
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   Category.findOne({
+    where: {
+      id: req.params.id
+    },
     include: {
       model: Product,
     }
+  })
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
   })
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then(dbUserData => res.json(dbUserData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    individualHooks: true,
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbUserData => {
+      if(!dbUserData[0]) {
+        res.status(404).json({ message: 'No user found with this id'});
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbUserData => {
+    if(!dbUserData[0]) {
+      res.status(404).json({ message: 'No user found with this id'});
+      return;
+    }
+    res.json(dbUserData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
