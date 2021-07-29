7246c7b69ded6e6dda132c87552ed844f6109081
j.connor.obrien@gmail.com <j.connor.obrien@gmail.com>
2021-07-29 16:42:38 -0400

    Added Tag Delete and Edit

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -51,10 +51,41 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(
+    {
+      tag_name: req.body.tag_name
+    },
+    {
+    where: {
+      id: req.params.id
+    },
+    
+  })
+  .then(dbCommentData => res.json(dbCommentData))
+  .catch(err => {
+    console.log(err);
+    res.status(400).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  }) 
+  .then(dbPostData => {
+    if (!dbPostData) {
+      res.status(404).json({ message: 'No tag found with this id' });
+      return;
+    }
+    res.json(dbPostData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
